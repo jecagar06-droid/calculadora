ce891af711284a338b2d7fdc331cca254b66d6b3
jecagar06-droid <jecagar06@gmail.com>
2025-11-15 19:23:19 -0500

    Add files via upload

---
+++ js/calculadora2.js
@@ -0,0 +1,33 @@
+const miFormulario = document.getElementById("calculadora");
+
+miFormulario.addEventListener("submit", function(event) {
+  // evita la recarga de la página
+  event.preventDefault();
+
+  const numero1 = document.getElementById("numero1");
+  const numero2 = document.getElementById("numero2");
+  const operacion = document.getElementById("operacion");
+
+  console.log("los numeros son:", numero1.value, "-", numero2.value);
+  const resultodo = document.getElementById("calculo");
+  resultodo.hidden = false;
+
+  const n1 = parseFloat(numero1.value);
+  const n2 = parseFloat(numero2.value);
+
+  let res;
+
+  if (operacion.value === "sumar") {
+    res = n1 + n2;
+  } else if (operacion.value === "restar") {
+    res = n1 - n2;
+  } else if (operacion.value === "multiplicar") {
+    res = n1 * n2;
+  } else if (operacion.value === "dividir") {
+    res = n2 !== 0 ? (n1 / n2).toFixed(2) : "Error: división entre 0";
+  } else {
+    res = "Operación no reconocida";
+  }
+
+  resultodo.textContent = "Resultado: " + res;
+});
